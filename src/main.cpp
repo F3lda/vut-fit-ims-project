be1808786ec7937fae2655152c2e52e9066b505e
Michael Tran <michaeltran262@gmail.com>
2022-11-30 21:30:15 +0100

    Add source files

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <simlib.h>
+#include "delivery.h"
+
+int main() {                    // experiment description
+  SetOutput("ims.dat");
+  Print("# Zásilkovna --- model of delivery service\n");
+  return 0;
+}
